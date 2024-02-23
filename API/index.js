4b266c037f10abee0183691b99fd678dfafa210a
Dongjin <lidongjin1990@gmail.com>
2024-02-22 23:22:08 -0800

    First commit

---
+++ API/index.js
@@ -0,0 +1,49 @@
+import express from "express";
+import { PORT } from "./config.js";
+import mongoose from "mongoose";
+import { Team } from "./models/teamModel.js";
+import teamRoute from './routes/teamRoute.js';
+import cors from 'cors';
+import {mongoDBURL} from './secret.js';
+
+
+const app = express();
+
+//Middleware for parsing request body
+app.use(express.json());
+
+//Middleware for handling cors policy
+//allow all origins with defalt of cors
+app.use(cors());
+// All custom origins only (better)
+/** 
+app.use(
+    cors({
+        origin:'http://localhost:3000',
+        method: ['GET', 'POST', 'PUT','DELETE'],
+        allowedHeaders: ['Content-Type'],
+    })
+);
+*/
+
+app.get('/', (request, response) => {
+    console.log(request)
+    return response.status(234).send('Welcome');
+});
+
+//Middle ware
+app.use('/teams',teamRoute);
+
+
+
+mongoose
+.connect(mongoDBURL)
+.then(() => {
+    console.log('App connected to database');
+    app.listen(PORT, () => {
+        console.log(`APP is listening to port: ${PORT}`);
+    });
+})
+.catch((error) => {
+    console.log(error);
+});
